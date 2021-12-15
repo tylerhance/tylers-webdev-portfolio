6f6dcb742ed1c16e0890823e1c426a726d09c348
Tyler Hance <tyler.hance@gmail.com>
2021-12-14 16:59:04 -0800

    Adding hero section with title and quick about me. Added learn more button to redirect to github profile.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Tyler Hance <br />
+        Full-Stack Web Developer
+      </SectionTitle>
+      <SectionText>
+        I am a full-stack web developer with a passion for problem solving and design. I specialize in the MERN stack (MongoDB, Express, React, Node), but I am always open to learning new technologies. 
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/tylerhance' }>Learn More</Button>
+    </LeftSection>
+  </Section>
+  
 );
 
 export default Hero;
